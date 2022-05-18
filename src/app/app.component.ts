ea6865507f389be188451cf56fb4de09f43f7ef0
Pham Anh Hung <hero.ulis.50@gmail.com>
2022-05-18 09:37:13 +0700

    feat: Les2 - Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,64 @@ export class AppComponent {
     {id: 4, name: 'Nguyen Van D', age: 35, gender: 1,avatar: 'https://source.unsplash.com/random', status: 1},
     {id: 5, name: 'Nguyen Van E', age: 26, gender: 1,avatar: 'https://source.unsplash.com/random', status: 0},
   ]
+  showItem = (object : Teacher)=> {
+    console.log('Hello');
+    console.log('Clicked ',object);
+  }
+  buttonState = true;
+  newTeacher = {
+     id: '',
+     name: '',
+     age: '',
+     gender: '',
+     avatar: '',
+     status: '',
+  }
+  toggleState() {
+    this.buttonState = !this.buttonState
+  }
+  handleInput(event: any, key: 'name' | 'age' | 'gender'|'avatar'|'status') {
+    this.newTeacher[key] = event.target.value;
+  }
+  onSubmit() {
+    this.teachers.push( {
+      ...this.newTeacher,
+      id: this.teachers.length + 1,
+      name: this.newTeacher.name,
+      age: +this.newTeacher.age,
+      gender: +this.newTeacher.gender,
+      avatar: 'https://source.unsplash.com/random',
+      status: +this.newTeacher.status,
+    });
+    console.log(this.newTeacher);
+    this.newTeacher = {
+      id: '',
+      name: '',
+      age: '',
+      gender: '',
+      avatar: '',
+      status: '',
+   }
+  }
+  fileToUpload: File | null = null;
+  handleFileInput(event:any) {
+    this.fileToUpload = event.target.files.item(0);
+    console.log(this.fileToUpload?.name);
+    
+  }
+  // uploadFileToActivity() {
+  //   this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+  //     // do something, if upload success
+  //     }, error => {
+  //       console.log(error);
+  //     });
+  // }
+}
+export type Teacher = {
+  id: number | string,
+  name: string,
+  age: number,
+  gender: number,
+  avatar: string,
+  status: number
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -9,6 +9,10 @@ export class TableComponent implements OnInit {
 
   constructor() { }
   ngOnInit(): void {
+    
+  }
+  handleClick() {
+    console.log('Hello');
     
   }
   @Input('users') teachers: any;
